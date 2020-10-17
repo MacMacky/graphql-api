fb9be4dfc380e2fd5dc3d09511433eff3d6d6d31
Mark A <mac21macky@gmailcom>
2020-10-18 07:11:47 +0800

    Modify logic 'main' file

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import './modules'
+import createServer from './server'
+
+const start = async () => {
+  try {
+    const server = await createServer()
+    const { url } = await server.listen();
+    console.log(`GraphQL Server listening at: ${url}`)
+  } catch (e) {
+    console.log(e);
+    process.exit(1)
+  }
+}
+
+start()
