769d0872ed9cc6ee8fa1a3486f03e39afa0dbbdb
Mark A <mac21macky@gmailcom>
2020-10-19 10:47:44 +0800

    Add new global functionalities

--- src/modules.ts
+++ src/modules.ts
@@ -1,4 +1,6 @@
 import Bluebird from 'bluebird'
+import path from 'path'
+import _fs from 'fs'
 import rethinkdbdash from 'rethinkdbdash'
 import RethinkQuery from './utils/RethinkQuery'
 
@@ -8,3 +10,7 @@ import RethinkQuery from './utils/RethinkQuery'
 global.Bluebird = Bluebird
 global.r = rethinkdbdash({ silent: true })
 global.Query = new RethinkQuery()
+global.path = path
+global.fs = _fs.promises
+global.log = (...args: (any | string)[]) => console.log(...args)
+
