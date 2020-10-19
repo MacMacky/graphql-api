c18173dae9cce0441d8e732a578e6e3d5ab7fd04
Mark A <mac21macky@gmailcom>
2020-10-19 16:06:06 +0800

    Modify logic in 'db'

--- src/db/index.ts
+++ src/db/index.ts
@@ -8,7 +8,7 @@ export default () => {
           // check if db already exists
           if (dbs.includes(data.db)) {
             log('[db] already exists skipping creation...')
-            return [true, data.db, data.tables];
+            return [true, data.tables];
           }
 
           // if not create db
@@ -16,34 +16,40 @@ export default () => {
           return r
             .dbCreate(data.db)
             .run()
-            .then(result => [result.dbs_created, data.db, data.tables])
+            .then(result => [result.dbs_created, data.tables])
         })
     )
     .then(result => {
-      const [created, db, tables] = result as [number, string, Table[]]
+      const [created, tables] = result as [number, Table[]]
       if (created) {
         // get existing tables
-        const tablesToBeCreated = r.db(db)
+        return Query
           .tableList()
-          .run() // filter existing tables to new existing tables in 'structure.json'
-          .then(existingTables => tables.filter(table => !existingTables.includes(table.name)))
-
-        // create new tables
-        return Bluebird.map(tablesToBeCreated, table =>
-          r.db(db)
-            .tableCreate(table.name)
-            .run()
-            .then(result => {
-              if (result.tables_created) {
-                return Bluebird.map(table.indexes, index => {
-                  return r.db(db)
-                    .table(table.name)
-                    .indexCreate(index)
-                    .run()
-                })
+          // filter existing tables to new existing tables in 'structure.json'
+          .then(existingTables => {
+            // old filter logic
+            // const tablesToBeCreated = tables.filter(table => !existingTables.includes(table.name))
+            // create new tables
+            return Bluebird.map(tables, table => {
+              // check if current table already exist in the `existingTables`
+              if (existingTables.includes(table.name)) {
+                return Query
+                  .indexList(table.name)
+                  .then(old_indexes => table.indexes.filter(index => !old_indexes.includes(index)))
+                  .then(indexes_to_create => Bluebird.map(indexes_to_create, index => Query.indexCreate(table.name, index)))
+              } else {
+                // if not create table
+                return Query
+                  .tableCreate(table.name)
+                  .then(result => {
+                    // if table is created create indexes for that table
+                    if (result.tables_created) {
+                      return Bluebird.map(table.indexes, index => Query.indexCreate(table.name, index))
+                    }
+                  })
               }
             })
-        )
+          })
       }
 
       return Promise.reject('[db] db was not created...')
