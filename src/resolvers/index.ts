7b47ffe2fe4d1ab70e6ca4a0850bc0d62d813725
Mark A <mac21macky@gmailcom>
2020-10-19 16:09:29 +0800

    Modify

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,10 +1,11 @@
 export default Bluebird.reduce(fs.readdir(__dirname), async (resolvers, module) => {
   const file = module.split(/\.(js|ts)/).shift() as string
+  // check if file is not 'index.js or index.ts'
   if (!file.includes('index')) {
     return {
       ...resolvers,
       [file]: (await import(path.join(__dirname, module))).default
     }
   }
-  return { ...resolvers }
+  return resolvers
 }, {})
