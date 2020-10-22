906cce322f220f4a8997e492f6d4a18139580811
Mark A <mac21macky@gmailcom>
2020-10-22 11:17:22 +0800

    Modify server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,17 @@ import './modules'
 import { ApolloServer } from 'apollo-server'
 import typeDefs from './schema'
 import resolvers from './resolvers'
+import utils from './utils'
 
 export default async () => {
   return new ApolloServer({
     typeDefs,
-    resolvers: await resolvers
+    resolvers: await resolvers,
+    context() {
+      return {
+        utils
+      }
+    }
   })
 }
 
