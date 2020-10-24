3b2b27b14894fb8d8a48d23bd673e9e15dfc5f72
Mark A <mac21macky@gmailcom>
2020-10-24 14:14:12 +0800

    Modify 'module' use dotenv functionality

--- src/modules.ts
+++ src/modules.ts
@@ -3,12 +3,16 @@ import path from 'path'
 import _fs from 'fs'
 import rethinkdbdash from 'rethinkdbdash'
 import RethinkQuery from './utils/RethinkQuery'
+import { config } from 'dotenv'
+
+// get all config
+config({ path: path.resolve(__dirname, '../.env'), debug: true })
 
 /*
  Make global variables here
 */
 global.Bluebird = Bluebird
-global.r = rethinkdbdash({ silent: true, db: 'university' })
+global.r = rethinkdbdash({ silent: true, db: process.env.DB })
 global.Query = new RethinkQuery()
 global.path = path
 global.fs = _fs.promises
