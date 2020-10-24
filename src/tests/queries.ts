e329623180038fa3dd4822d6847168fb5c7ed1ba
Mark A <mac21macky@gmailcom>
2020-10-24 14:12:16 +0800

    Add new graphql queries

--- src/tests/queries.ts
+++ src/tests/queries.ts
@@ -41,7 +41,6 @@ const CREATE_CLASS = gql`
     }
 `
 
-
 const GET_STUDENTS = gql`
   query getStudents {
     result: students {
@@ -51,10 +50,81 @@ const GET_STUDENTS = gql`
     }
   }
 `
+
+const GET_CLASSES = gql`
+  query getClasses {
+    result: classes {
+      id
+      subject_id
+      teacher_id
+      start_time
+      subject {
+        id
+        subject_name
+      }
+    }
+  }
+`
+
+const GET_ENROLLMENT = gql`
+  query getEnrollments {
+    result: enrollments {
+      id 
+      student_id
+      subject_id
+    }
+  }
+`
+const GET_TEACHERS = gql`
+  query getTeachers {
+    result: teachers {
+      id
+      first_name
+      last_name
+      middle_name
+      department_id
+    }
+  }
+`
+const GET_DEPARTMENTS = gql`
+    query getDepartments {
+      result: departments {
+        id
+        name
+      }
+    }
+`
+
+const GET_COURSES = gql`
+  query getCourses {
+    result: courses {
+      id
+      course_name
+      year_level
+    }
+  }
+`
+
+const GET_SUBJECTS = gql`
+  query getCourses {
+    result: subjects {
+      id
+      subject_name
+      credits
+    }
+  }
+`
+
 export {
   CREATE_STUDENT,
   ENROLL_CLASS,
   CREATE_TEACHER,
   CREATE_CLASS,
-  GET_STUDENTS
+  GET_STUDENTS,
+  GET_TEACHERS,
+  GET_ENROLLMENT,
+  GET_CLASSES,
+  GET_DEPARTMENTS,
+  GET_COURSES,
+  GET_SUBJECTS
 }
